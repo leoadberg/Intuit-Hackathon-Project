0a1bab0f3ae63909bbcb325532ec5f5e8cc54e3d
Leo Adberg <ladberg@berkeley.edu>
2016-09-17 13:18:54 -0700

    Updated a bunch of stuff

---
+++ GetAllData.sh
@@ -0,0 +1,10 @@
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'XYZ=($(cat data/listOfCodes.txt))'
+years=("2002" "2007" "2012")
+for year in "${years[@]}"
+do
+for i in "${XYZ[@]}"
+do
+echo ${i}
+curl "http://api.census.gov/data/"${year}"/ewks?get=PAYANN,ESTAB,EMP&for=county:*&in=state:*&NAICS"${year}"=$i&OPTAX=A&key=3d4047590356727bf89cab9124ab1340c179068c" > data/${year}/${i}.txt
+done
+done
--- GetData.py
+++ GetData.py
@@ -1,20 +1,127 @@
 import urllib.request
 import json
+from numpy import genfromtxt
+import locale
+import sys
+import io
 
 api_key = "3d4047590356727bf89cab9124ab1340c179068c"
 
-dataToGet = "EMP,NAICS2012_TTL,OPTAX,GEO_TTL"
-state = "02"
-county = "016"
-NAICS = "54" #Professional, Scientific, and Technical Services
+states = []
+naics = []
+minSalary = [0]
+maxSalary = [100000000]
+prediction = [10]
 
-#response = urllib2.urlopen('http://api.census.gov/data/2012/ewks?get=EMP,NAICS2012_TTL,OPTAX,GEO_TTL&for=county:016&in=state:02&NAICS2012=31-33')
-URL = 'http://api.census.gov/data/2012/ewks?get='+dataToGet+'&for=county:'+county+'&in=state:'+state+'&NAICS2012='+NAICS+'&key='+api_key
+# Code to turn a state/county name into a FIPS code and back
+locale.setlocale(locale.LC_ALL, 'en_US')
+stateCodes = genfromtxt("codes/FIPS.txt", dtype="str", delimiter="	")
+countyCodes = genfromtxt("codes/Counties.txt", dtype="str", delimiter="	")
+industryCodes = genfromtxt("codes/NAICS.txt", dtype="str", delimiter="	")
+def FIPSToCounty(state, county):
+	for i in range(len(countyCodes)):
+		if countyCodes[i][0] == state:
+			if int(countyCodes[i][1]) == int(county):
+				return countyCodes[i][2]
+	#print("Could not get county "+county+" in state "+state)
+	return "Unknown County"
+def FIPSToState(fips):
+	for i in range(len(stateCodes)):
+		if stateCodes[i][0] == fips:
+			return stateCodes[i][1]
+def StateToFIPS(state):
+	for i in range(len(stateCodes)):
+		if stateCodes[i][1] == state:
+			return stateCodes[i][0]
+def NAICSToIndustry(NAICS):
+	for i in range(len(industryCodes)):
+		if industryCodes[i][0] == NAICS:
+			return industryCodes[i][1]
+
+# Parse arguments and put them into the right lists
+currentSelected = None
+for arg in sys.argv:
+	if arg == "GetData.py":
+		pass
+	elif arg == "-state":
+		currentSelected = states
+	elif arg == "-NAICS":
+		currentSelected = naics
+	elif arg == "-minSalary":
+		currentSelected = minSalary
+	elif arg == "-maxSalary":
+		currentSelected = maxSalary
+	elif arg == "-prediction":
+		currentSelected = prediction
+	else:
+		currentSelected.append(arg)
+minSalary = int(minSalary[-1])
+maxSalary = int(maxSalary[-1])
+prediction = int(prediction[-1])
+
+
+# Set up API query
+#dataToGet = "PAYANN,ESTAB,EMP,NAICS2012_TTL,GEO_TTL"
+#stateToSearch = "06" #California
+#countyToSearch = "*" #Alameda
+#NAICSToSearch = "54" #Professional, Scientific, and Technical Services
+#URL = 'http://api.census.gov/data/2012/ewks?get='+dataToGet+'&for=county:'+countyToSearch+'&in=state:'+stateToSearch+'&NAICS2012='+NAICSToSearch+'&OPTAX=A&key='+api_key
 #print(URL)
-request = urllib.request.Request(URL)
-response = urllib.request.urlopen(request)
-encoding = response.info().get_content_charset('utf8')
-output = json.loads(response.read().decode(encoding))
+#request = urllib.request.Request(URL)
+#response = urllib.request.urlopen(request)
+#encoding = response.info().get_content_charset('utf8')
+#output = json.loads(response.read().decode(encoding))
+
+statesToSearch = [StateToFIPS(x) for x in states]
+allData = {}
+
+for code in naics:
+	with open('data/2012/'+code+".txt") as data_file:    
+		data = json.load(data_file)
+		allData[code] = data
+
+#print(allData)
+#0/0
+
+finalData = []
+
+PAYANN = list(allData.values())[0][0].index('PAYANN')
+STATE = list(allData.values())[0][0].index('state')
+COUNTY = list(allData.values())[0][0].index('county')
+EMP = list(allData.values())[0][0].index('EMP')
+for code in allData:
+	for x in allData[code][1:]:
+		#state = FIPSToState(x[output[0].index('state')])
+		#print(x)
+		if int(x[EMP]) == 0:
+			averagePay = 0
+		else:
+			averagePay = int(x[PAYANN]) * 1000 / int(x[EMP])
+		if averagePay >= minSalary and averagePay <= maxSalary:
+			thisEntry = []
+			state = FIPSToState(x[STATE])
+			county = FIPSToCounty(x[STATE], x[COUNTY])
+			#Industry = x[allData[code][0].index('NAICS2012_TTL')]
+			Industry = "temp"
+			#print("In state: "+state)
+			#print("In county: "+county)
+			#print("Industry: "+Industry)
+			#print("Average pay: $"+locale.format("%d", averagePay, grouping=True))
+			thisEntry.append(state)
+			thisEntry.append(county)
+			thisEntry.append(Industry)
+			thisEntry.append(averagePay)
+			finalData.append(thisEntry)
+
+# Sort all entries by average pay
+finalData = sorted(finalData, key = lambda x: -x[3])
+
+#print(finalData)
+
+for entry in finalData:
+	print("In state: "+entry[0])
+	print("In county: "+entry[1])
+	print("Industry: "+entry[2])
+	print("Average pay: $"+locale.format("%d", entry[3], grouping=True))
 
 
-print(output)
